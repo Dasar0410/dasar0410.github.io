f9f3cfee98b29c12467838ab17dde07ed63fd1b6
Dasar0410 <danielsarjomaa@gmail.com>
2024-02-24 16:54:46 +0100

    Update: CSS

--- src/components/intro.js
+++ src/components/intro.js
@@ -4,13 +4,12 @@ import portrait from './img/portrait.jpg'
 const intro = () => {
   return (
     <section id='intro'>
-        <div className='IntroContent'>
-        <span className='Hei'>Hei,</span>
-        <span className='introText'>Jeg heter <span className='introName'>Daniel</span> <br/>Cybersikkerhet student</span>
+        <div className='introContent'>
+        <span className='hello'>Hei,</span>
+        <span className='introText'> Jeg heter <span className='introName'>Daniel</span> <br/>Cybersikkerhet student</span>
         <p className='introPara'>Jeg er en tredjeårsstudent på Digital Infrastruktur og Cybersikkerhet i Gjøvik</p>
         </div>
-        <img src={portrait} alt='portrait'></img>
-
+        <img src={portrait} alt='portrait' className='portrait'></img>
     </section>
   )
 }
