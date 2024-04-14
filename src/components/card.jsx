266674b3f750c50cc91e11f045021aec18f3eea9
Dasar0410 <danielsarjomaa@gmail.com>
2024-04-14 18:46:54 +0200

    added tailwind.css

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,7 +4,7 @@ import project from './img/empty.png'
 
 const card = () => {
     return (
-        <div className='card'>
+        <div className='card transition ease-in-out hover:-translate-y-1 hover:scale-110'>
         <img className="card-img" src={project} alt='projectimg'></img>
         <h2>Prosjekt1</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus finibus enim at odio sagittis hendrerit. Donec risus erat, imperdiet quis fermentum id, tincidunt id orci. Duis tempor consectetur tellus id. </p>
