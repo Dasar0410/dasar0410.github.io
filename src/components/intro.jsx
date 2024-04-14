560c2c9da06e95fcc5ea1f5e1e2dcbf85b517348
Dasar0410 <danielsarjomaa@gmail.com>
2024-04-14 18:50:41 +0200

    Changed text size and coloring intro

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -5,9 +5,9 @@ const intro = () => {
   return (
     <section id='intro'>
         <div className='introContent'>
-        <span className='hello'>Hei,</span>
-        <span className='introText'> Jeg heter <span className='introName'>Daniel.</span> </span>
-        <span className='work'>Student</span>
+        <span className='hello'>Hei, Jeg heter</span>
+        <span className='introText'>Daniel  <span className='introName'> Sarjomaa</span> </span>
+        <span className='work'></span>
         <p className='introPara'>Bachelor i Digital infrastruktur og cybersikkerhet<br/>NTNU Gjøvik</p>
         <img src={portrait} alt='portrait' className='portrait'></img>
         </div>
