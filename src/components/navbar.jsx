fff6724dccccc72a58bb85f9001fb666ca64076d
Dasar0410 <danielsarjomaa@gmail.com>
2024-04-14 20:13:30 +0200

    added hamburger menu for mobile

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import './navbar.css'
 import contactImg from './img/contact.png'
 import { Link } from 'react-scroll'
 
 const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false);
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    }
     return (
+        <>
         <nav className="navbar">
         <div className="S">S.</div>
-            <div className="desktopMenu">
+            <div className={`desktopMenu ${showMenu ? 'hide' : ''}`}>
                 <Link className="desktopMenuListItem">Home</Link>
                 <Link className="desktopMenuListItem">About</Link>
                 <Link className="desktopMenuListItem">Portfolio</Link>
@@ -16,8 +21,23 @@ const Navbar = () => {
             <button className="desktopMenuBtn">
                 <img src={contactImg} alt="Contact_Me" className="desktopMenuImg"></img> Contact me
             </button>
+            <button className="desktopMenuBtn" onClick={toggleMenu}>
+                <ion-icon name="menu"></ion-icon>
+                    </button>
         </nav>
+
+
+        <div className={`mobileMenu ${showMenu ? 'visible' : 'invisible'}`}>
+                <Link className="mobileMenuItem">Home</Link>
+                <Link className="mobileMenuItem">About</Link>
+                <Link className="mobileMenuItem">Portfolio</Link>
+                <Link className="mobileMenuItem">Certificates</Link>
+            </div>
+        </>
+        
     )
+    
 }
+
 
 export default Navbar
