4714c31079bd1c7eb7cc7dc66e908c2ec5675b91
Dasar0410 <danielsarjomaa@gmail.com>
2024-04-23 22:26:02 +0200

    Heatmap component imported and API data visualized

--- src/components/github.jsx
+++ src/components/github.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import HeatMap from '@uiw/react-heat-map';
+
+import './github.css' 
 
 const Github = () => {
   const [commitsByDay, setCommitsByDay] = useState([]);
@@ -18,14 +21,19 @@ const Github = () => {
         const groupedData = commitData.reduce((acc, commit) => {
           const date = commit.date.split('T')[0];  // Assuming date is in ISO format and includes time
           if (!acc[date]) {
-            acc[date] = { date, count: 0 };
+            acc[date] = 0;
           }
-          acc[date].count += commit.count;
+          acc[date] += commit.count;
           return acc;
         }, {});
 
-        // Convert grouped data object to array
-        setCommitsByDay(Object.values(groupedData));
+        // Convert grouped data object to array required by HeatMap
+        const heatMapData = Object.keys(groupedData).map(date => ({
+          date: date.replace(/-/g, '/'), // Date converted from European to American
+          count: groupedData[date]
+        }));
+
+        setCommitsByDay(heatMapData);
       } catch (error) {
         setError(error.message);
       } finally {
@@ -40,14 +48,33 @@ const Github = () => {
   if (error) return <div>Error: {error}</div>;
 
   return (
-    <div>
-      <h1>GitHub Commit Data</h1>
-      {commitsByDay.map(({ date, count }, index) => (
-        <div key={index}>
-          Date: {date}, Number of Commits: {count}
-        </div>
-      ))}
+    <>
+    
+    <div className='gitHeatmap flex justify-center'>
+    
+      <HeatMap
+        value={commitsByDay}
+        weekLabels={['', 'Mon', '', 'Wed', '', 'Fri', '']}
+        startDate={new Date(new Date().getFullYear(), 0, 1)} // Start from the beginning of the current year
+        style={{ '--rhm-rect': '#b9b9b9' }}
+        rectSize={16}
+        width={350}
+        legendRender={(props) => <rect {...props} y={props.y + 10} rx={2} />}
+        rectProps={{
+          rx: 2
+        }}
+        panelColors={{
+        
+        0: '#100F2E',
+        2: '#ffdd9f',
+        4: '#ffdd9f',
+        10: '#ffc660',
+        20: '#ffb633',
+        30: '#ffb633',
+        }}
+      />
     </div>
+    </>
   );
 };
 
