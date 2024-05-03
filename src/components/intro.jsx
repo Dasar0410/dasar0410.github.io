4a11b4b089e2e705e4cebce7981fe1d7a62ce59d
Dasar0410 <danielsarjomaa@gmail.com>
2024-05-03 21:27:16 +0200

    added more proffesional placeholder for portrait

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './intro.css'
-import portrait from './img/placeholder.jpg'
+import portrait from './img/placeholder.svg'
 const intro = () => {
   return (
     <section id='intro'>
