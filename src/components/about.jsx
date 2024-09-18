11478f6f4975ca848e91a232128df44bd31da7cf
Dasar0410 <danielsarjomaa@gmail.com>
2024-09-19 00:34:21 +0200

    navbar is prettier, cleaned up code

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,10 +4,13 @@ import './about.css'
 const about = () => {
     return (
         <section id='about' className='about'>
+        <div className='gridAbout'>
             <div className='aboutContainer'>
-            <h1 className='aboutTitle'>About</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mattis erat non aliquet volutpat. Aliquam tempus laoreet bibendum. Donec venenatis augue eget tortor pulvinar viverra. Duis finibus imperdiet enim non ultrices. Morbi lobortis malesuada magna ac dictum. Duis eu efficitur enim, a porta erat. Donec vel mattis lectus. Aenean egestas mattis facilisis. Morbi eu orci nec felis egestas iaculis. Nunc gravida mattis nisl, a elementum enim aliquam nec. Nam mattis nisi scelerisque fermentum bibendum. Sed vitae ornare sapien.</p>
-            
+                <h1 className='aboutTitle'>About</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mattis erat non aliquet volutpat. Aliquam tempus laoreet bibendum. Donec venenatis augue eget tortor pulvinar viverra. Duis finibus imperdiet enim non ultrices. Morbi lobortis malesuada magna ac dictum. Duis eu efficitur enim, a porta erat. Donec vel mattis lectus. Aenean egestas mattis facilisis. Morbi eu orci nec felis egestas iaculis. Nunc gravida mattis nisl, a elementum enim aliquam nec. Nam mattis nisi scelerisque fermentum bibendum. Sed vitae ornare sapien.</p>
+            </div>
+            <div className='techContainer'>
+                </div>
             </div>
         </section>
     )
