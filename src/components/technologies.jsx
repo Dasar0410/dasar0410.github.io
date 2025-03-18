ae1868cf9ebd5ef0e42d2de81a484d590a09ae1f
Dasar0410 <danielsarjomaa@gmail.com>
2025-03-18 08:40:51 +0100

    updated icons

--- src/components/technologies.jsx
+++ src/components/technologies.jsx
@@ -9,32 +9,32 @@ const technologies = () => {
             <div className="category">
                 <h2 className="category-header small-header">Frontend</h2>
                 <div className="icons-container">
-                        <StackIcon name="html5" className="tech-icon" />      
-                        <StackIcon name="css3" className="tech-icon" />
-                        <StackIcon name="reactjs" className="tech-icon" />
-                        <StackIcon name="js" className="tech-icon" />
-                        <StackIcon name="typescript" className="tech-icon" />
-                        <StackIcon name="vitejs" className="tech-icon" />
-                        <StackIcon name="tailwindcss" className="tech-icon" />
+                    <StackIcon name="html5" className="tech-icon" title="HTML5" />
+                    <StackIcon name="css3" className="tech-icon" title="CSS3" />
+                    <StackIcon name="reactjs" className="tech-icon" title="ReactJS" />
+                    <StackIcon name="js" className="tech-icon" title="JavaScript" />
+                    <StackIcon name="typescript" className="tech-icon" title="TypeScript" />
+                    <StackIcon name="vitejs" className="tech-icon" title="ViteJS" />
+                    <StackIcon name="tailwindcss" className="tech-icon" title="TailwindCSS" />
                 </div>
             </div>
             <div className="category">
                 <h2 className="category-header small-header">Backend</h2>
                 <div className="icons-container">
-                    <StackIcon name="java" className="tech-icon" />
-                    <StackIcon name="c++" className="tech-icon" />
-                    <StackIcon name="python" className="tech-icon" />
-                    <StackIcon name="nodejs" className="tech-icon" /> 
-                    <StackIcon name="postgresql" className="tech-icon" />
-                    <StackIcon name="supabase" className="tech-icon" />
+                    <StackIcon name="java" className="tech-icon" title="Java" />
+                    <StackIcon name="c++" className="tech-icon" title="C++" />
+                    <StackIcon name="python" className="tech-icon" title="Python" />
+                    <StackIcon name="nodejs" className="tech-icon" title="NodeJS" />
+                    <StackIcon name="postgresql" className="tech-icon" title="PostgreSQL" />
+                    <StackIcon name="supabase" className="tech-icon" title="Supabase" />
                 </div>
             </div>
             <div className="category">
                 <h2 className="category-header small-header">DevOps</h2>
                 <div className="icons-container">
-                    <StackIcon name="cloudflare" className="tech-icon" />
-                    <StackIcon name="docker" className="tech-icon" />
-                    <StackIcon name="git" className="tech-icon" />
+                    <StackIcon name="cloudflare" className="tech-icon" title="Cloudflare" />
+                    <StackIcon name="docker" className="tech-icon" title="Docker" />
+                    <StackIcon name="git" className="tech-icon" title="Git" />
                 </div>
             </div>
         </div>
